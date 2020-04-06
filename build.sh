557d27d99ab23bf92cfcd203736f85d4ada8d439
neo.tian <neo.tian@dadaabc.net>
2020-04-06 13:45:11 +0800

    fix build.sh bug

--- build.sh
+++ build.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-function find_project_name {
-	project="eee"
-	for((i=3;i<=10;i++));  
-	do   
-		prev="a"
-		tmp=`pwd | cut -d '/' -f $i`
-		if [ -n $tmp ]; then
-			prev=$tmp
-		fi
-		echo $prev
-	done  
-}
-
-
-#mvn clean install -Dmaven.test.skip=true
+project="jxplorer"
+echo "$project"
 curdir=`pwd`
 
-project=`find_project_name`
-echo "project is $project"
+mvn clean install -Dmaven.test.skip=true
+
 rm -rf dist
 mkdir dist
 appclass="org.springframework.boot.loader.JarLauncher"
 srcfiles="$curdir/target/jexplorer-0.0.1-SNAPSHOT.jar"
 outdir="$curdir/dist/"
-cmd="javapackager -deploy -native image -appclass $appclass -srcfiles $srcfiles -outdir $outdir -outfile $project -name $project"
-`$cmd`
+javapackager -deploy -native image -appclass $appclass -srcfiles $srcfiles -outdir $outdir -outfile $project -name $project
